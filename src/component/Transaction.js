3cb8d5c4259489671016767fe33b6b7be48890b9
jahedripon9 <jahedahmed3170@gmail.com>
2022-07-10 01:43:58 +0600

    Updating Balance, Income & Expenses

--- src/component/Transaction.js
+++ src/component/Transaction.js
@@ -1,11 +1,14 @@
 import React from 'react'
 
 export const Transaction = ({transaction}) => {
+
+    const sign = transaction.amount < 0 ? '-' : '+';
+
     return (
         <div>
-            <li className="minus">
+            <li className={transaction.amount < 0 ? 'minus' : 'plus'}>
                 {transaction.text}
-                <span>-$400</span><button className="delete-btn">x</button>
+                <span>{sign}${Math.abs(transaction.amount)}</span><button className="delete-btn">x</button>
             </li>
         </div>
     )
